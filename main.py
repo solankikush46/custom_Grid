1013d7268e95148cb35d280c7c03a9c2b266ab64
ctschwandt <bigbrovc@gmail.com>
2025-06-28 18:50:25 -0700

    sync

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 # main.py
 
 from test import *
+import os
+
+def ensure_directories_exist():
+    directories = [
+        LOGS["ppo"],
+        LOGS["dqn"],
+        MODELS["ppo"],
+        MODELS["dqn"],
+        FIXED_GRID_DIR,
+        RANDOM_GRID_DIR,
+    ]
+    for d in directories:
+        os.makedirs(d, exist_ok=True)
     
 if __name__ == "__main__":
+    ensure_directories_exist()
     #train_for_test_battery(50_000)
     #test_battery()
     #test_manual_control()
-    test_PPO(timesteps=1_000_000, rows=20, cols=20)
-    
+    #test_PPO(timesteps=1_000_000, rows=20, cols=20)
+    # 20x20 with 15% obstacles and 3 sensors
+    generate_grid(
+    rows=20,
+    cols=20,
+    obstacle_percentage=0.15,
+    n_sensors=3
+    )
+
+    # 20x20 with 30% obstacles and 3 sensors
+    generate_grid(
+    rows=20,
+    cols=20,
+    obstacle_percentage=0.30,
+    n_sensors=3
+    )
+
