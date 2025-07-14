fcbfb3c6ba545ac783dcebb6b8005cd6d1cc62a6
ctschwandt <bigbrovc@gmail.com>
2025-07-14 12:30:46 -0700

    sync

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ from train import *
 
 def ensure_directories_exist():
     directories = [
-        LOGS["ppo"],
-        MODELS["ppo"],
+        SAVE_DIR,
         FIXED_GRID_DIR
     ]
     for d in directories:
         os.makedirs(d, exist_ok=True)
 
 def main():
-    test_manual_control()
+    #test_manual_control()
     #generate_all_plots(rolling_window=50_000)
     #test_PPO(10_000, 20, 20)
     #test_PPO(300_000, 100, 100)
@@ -25,8 +24,7 @@ def main():
     #train_quick_junk_model("mine_20x20.txt")
     #test_render_junk_model("mine_20x20.txt")
     #train_and_render_junk_model()
-    #train_all_models(1_000_000)
-    #evaluate_all_models(n_eval_episodes=20, render=True)
+    train_all_models(1_000_000)
     #evaluate_all_models(n_eval_episodes=20, render=True)
     
 if __name__ == "__main__":
