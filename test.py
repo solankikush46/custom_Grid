97256524ffb701bc2e5c5f27ede89663c7b11b98
ctschwandt <bigbrovc@gmail.com>
2025-07-15 21:25:30 -0700

    sync

--- test.py
+++ test.py
@@ -150,7 +150,10 @@ def train_all_models(timesteps: int = 1_000_000):
     specified
     """
     models_to_train = [
-        {"grid_file": "mine_20x20.txt", "is_cnn": False, "model_name": "mine_20x20__reward_d", "halfsplit": False}
+         {"grid_file": "a_30x30.txt", "is_cnn": False, "model_name": "a_30x30__reward_d", "halfsplit": False},
+        {"grid_file": "b_30x30.txt", "is_cnn": False, "model_name": "b_30x30__reward_d", "halfsplit": False},
+        {"grid_file": "c_30x30.txt", "is_cnn": False, "model_name": "c_30x30__reward_d", "halfsplit": False},
+         {"grid_file": "a_50x50.txt", "is_cnn": False, "model_name": "c_30x30__reward_d", "halfsplit": False},
     ]
 
     for config in models_to_train:
