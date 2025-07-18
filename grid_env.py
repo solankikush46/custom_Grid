ba6eed833983620a8222e07f15940aa5f8e0afe5
ctschwandt <bigbrovc@gmail.com>
2025-07-17 21:39:41 -0700

    sync

--- grid_env.py
+++ grid_env.py
@@ -237,7 +237,7 @@ class GridWorldEnv(Env):
         self.last_action = -1
         self.miners = []
         self.n_miners = 12
-        self.OBSTACLE_VALS = (OBSTACLE, SENSOR, BASE_STATION
+        self.OBSTACLE_VALS = (OBSTACLE, SENSOR, BASE_STATION)
         self.n_miners = n_miners
         
         # exclusively for graphing
