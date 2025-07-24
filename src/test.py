743d7534cbbc05fe2e6b7d841035f1a5bf174f31
ctschwandt <bigbrovc@gmail.com>
2025-07-23 18:00:44 -0700

    sync

--- src/test.py
+++ src/test.py
@@ -208,11 +208,23 @@ def train_all_models(timesteps: int = 1_000_000):
             "fallback": False,
             #"conf": 0.5
         },
-          {
+        {
             "grid_file": "mine_50x50.txt", "arch": None, "reward_fn": get_reward_pathlen, 
             "is_att": False,
+            "fallback": True,
+            "conf": 0.5
+        },
+        {
+            "grid_file": "mine_50x50.txt", "arch": "seq", "reward_fn": get_reward_pathlen, 
+            "is_att": True,
             "fallback": False,
             #"conf": 0.5
+        },
+         {
+            "grid_file": "mine_50x50.txt", "arch": "seq", "reward_fn": get_reward_pathlen, 
+            "is_att": True,
+            "fallback": True,
+            "conf": 0.5
         },
     ]
 
